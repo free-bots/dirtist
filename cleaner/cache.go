a057c5d3452e78b6ed633ea3da38e3c8d0684965
free-bots <free-bots.to@protonmail.com>
2020-05-03 19:08:13 +0200

    added struct

--- cleaner/cache.go
+++ cleaner/cache.go
@@ -7,7 +7,16 @@ import (
 
 // clear the .cache and other paths ... clear package cache from extra package -> packageManager
 
+type CacheItem struct {
+	IsDir    bool        `json:"is_dir"`
+	Name     string      `json:"name"`
+	Path     string      `json:"path"`
+	Children []CacheItem `json:"children"`
+	Size     uint64      `json:"size"`
+}
+
 func GetCacheSize() int64 {
+	// todo get from config or use default
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		fmt.Println(err)
